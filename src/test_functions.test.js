b84a569cda5723b4310c0f195eb99798085f6181
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-15 02:04:22 -0600

    Add lowercase string test

--- src/test_functions.test.js
+++ src/test_functions.test.js
@@ -3,3 +3,7 @@ import { capitalize } from './test_functions';
 test('Empty String', () => {
     expect(capitalize('')).toBe('');
 });
+
+test('Lowercase String', () => {
+    expect(capitalize('hello')).toBe('Hello');
+});

2caf9546b2edc9a053158341f7ec3e5d7f36c553
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-15 02:41:35 -0600

    Add calculator add/subtract tests

--- src/test_functions.test.js
+++ src/test_functions.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from './test_functions';
+import { capitalize, reverseString, Calculator } from './test_functions';
 
 describe('Capitalize Tests', () => {
     it('Empty String', () => {
@@ -21,5 +21,45 @@ describe('ReverseString Tests', () => {
 
     it('Handles Space', () => {
 	expect(reverseString('hello world')).toBe('dlrow olleh');
+    });
+});
+
+describe('Calculator Tests', () => {
+    const calculator = new Calculator();
+
+    describe('Add Tests', () => {
+	it('Add 0', () => {
+	    expect(calculator.add(2, 0)).toBe(2);
+	});
+	
+	it('Add 4 12', () => {
+	    expect(calculator.add(4, 12)).toBe(16);
+	});
+	
+	it('Add Negative Number', () => {
+	    expect(calculator.add(2, -2)).toBe(0);
+	});
+
+	it('Add To Negative', () => {
+	    expect(calculator.add(2, -4)).toBe(-2);
+	})
+    });
+
+    describe('Subtract Tests', () => {
+	it('Subtract 0', () => {
+	    expect(calculator.subtract(2, 0)).toBe(2);
+	});
+
+	it('Subtract 12 4', () => {
+	    expect(calculator.subtract(12, 4)).toBe(8);
+	});
+
+	it('Subtract To Negative', () => {
+	    expect(calculator.subtract(2, 4)).toBe(-2);
+	});
+
+	it('Subtract Negative Number', () => {
+	    expect(calculator.subtract(2, -2)).toBe(4);
+	});
     });
 });

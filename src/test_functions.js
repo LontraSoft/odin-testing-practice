a7bd6ee15b1dc572cd31e6fa58d3fb588f9a6fcd
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-02-15 02:22:51 -0600

    Add reverseString function

--- src/test_functions.js
+++ src/test_functions.js
@@ -5,3 +5,7 @@ export function capitalize(string) {
     
     return string[0].toUpperCase() + string.slice(1);
 }
+
+export function reverseString(string) {
+    return string.split('').reverse().join('');
+}
